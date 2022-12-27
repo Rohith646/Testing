81c437b28dd90a48b9948a6b7ca53e1ef91cae0f
MOHAN <MOHAN@DESKTOP-5BMEASS>
2022-12-27 15:38:15 +0530

    build no 4

---
+++ RestApi/src/Pojo/Singleuser.java
@@ -0,0 +1,14 @@
+package Pojo;
+
+public class Singleuser
+{
+    private Data data;
+    private Support support;
+	public Data getData() {
+		return data;
+	}
+	public Support getSupport() {
+		return support;
+	}
+    
+}
